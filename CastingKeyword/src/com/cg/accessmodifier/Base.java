a3b12d05a981524884cbc8003235f9648570046f
Chaithu <Chaithu@DESKTOP-5M9U056>
2022-08-19 20:02:17 +0530

    first commit

---
+++ CastingKeyword/src/com/cg/accessmodifier/Base.java
@@ -0,0 +1,31 @@
+package com.cg.accessmodifier;
+
+public class Base {
+	
+	int var_default;
+	public int var_public;
+	private int var_private;
+	protected int var_protected;
+	/*
+	* Declaring methods with default, public, private,
+	protected types
+	*/
+	void method_default()
+	{
+	System.out.println("Default access Base class");
+	}
+	public void method_public()
+	{
+	System.out.println("Public access Base class");
+	}
+	private void method_private()
+	{
+	System.out.println("Private access Base class");
+	}
+	protected void method_protected()
+	{
+	System.out.println("Protected access Base class");
+	}
+}
+
+
---
+++ CastingKeyword/src/com/cg/accessmodifier/Executor.java
@@ -0,0 +1,13 @@
+package com.cg.accessmodifier;
+
+public class Executor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Derived d=new Derived();
+		Unrelated u=new Unrelated();
+		}
+
+	}
+
+
