67329942285d236ea952bd5b5e209c947208d366
Chaithu <Chaithu@DESKTOP-5M9U056>
2022-08-28 19:35:20 +0530

    first commit

---
+++ AccessModifiers/src/access_modifiers_demo/test1.java
@@ -0,0 +1,16 @@
+package access_modifiers_demo;
+
+public class test1 {
+	public int i=10;
+	public void show() {
+		System.out.println("hello world");
+	}
+
+	public static void main(String[] args) {
+		test1 t1=new test1();
+		t1.show();
+		System.out.println(t1.i);
+
+	}
+
+}
---
+++ AccessModifiers/src/access_modifiers_demo/test2.java
@@ -0,0 +1,12 @@
+package access_modifiers_demo;
+
+public class test2 extends test1 {
+
+	public static void main(String[] args) {
+		test1 t2=new test1();
+		t2.show();
+		System.out.println(t2.i);
+
+	}
+
+}
