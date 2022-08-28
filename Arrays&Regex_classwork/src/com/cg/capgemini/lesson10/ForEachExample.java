dc668400097300e6faeeeffc19c46e304e9c5aeb
Chaithu <Chaithu@DESKTOP-5M9U056>
2022-08-28 20:08:51 +0530

    first commit

---
+++ Arrays&Regex_classwork/src/com/cg/capgemini/lesson10/ForEachExample.java
@@ -0,0 +1,14 @@
+package com.cg.capgemini.lesson10;
+
+public class ForEachExample {
+
+	public static void main(String[] args) {
+		char[] vowels = { 'a', 'e', 'i', 'o', 'u'};
+
+		for(char ch: vowels){
+		System.out.println(ch);
+		}
+
+	}
+
+}
